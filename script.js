b89724e68699244b0fcaa7635372453ac4701e12
ArteeHeartee <andreasrtee@gmail.com>
2026-07-27 14:38:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -65,3 +65,15 @@ window.addEventListener("resize", () => {
     closeMenu();
   }
 });
+const navbar = document.querySelector(".navbar");
+
+function updateNavbar() {
+  if (window.scrollY > 60) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+}
+
+updateNavbar();
+window.addEventListener("scroll", updateNavbar);
